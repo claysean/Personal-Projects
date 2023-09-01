cc8672cbe7f771581c7953f1c9667c140e3dddb2
SeanOnePointOh <44099305+claysean@users.noreply.github.com>
2023-08-31 22:32:59 -0700

    Work From Last Night
    Distracted by watch party

--- TTRPG_Code/Dist_File_Maker.py
+++ TTRPG_Code/Dist_File_Maker.py
@@ -1,11 +1,24 @@
-import numpy as np
-import pandas as pd
+dS = 4 #dice size
+nD = 2 #number of dice
 
-dice = 4
-noDice = 3
-dieResults = [1]
+def diceIt(dR, n):
+    if dR[n] == 
 
-for x in range(noDice+1):
-    dieResults = np.array([dieResults]*5)
+for x in range(nD):
+    if x == 0:
+        dR = [1]
+    else:
+        dR.append(1)
 
-print(dieResults)
+m = sum(dR) #minimum dice roll
+dDist = []
+n = 0
+
+while sum(dR) <= dS*nD:
+    dDist[sum(dR)-nD] += 1
+    if sum(dR) < dS*nD:
+        continue
+    else:
+        break
+
+print(dR)
