43654382d7b22e2d0f593e1a6044d46997ca0bd1
SeanOnePointOh <44099305+claysean@users.noreply.github.com>
2023-09-15 22:32:16 -0700

    Martial Class Base Stats
    Tables for martial base stats, general assumptions made about builds. (CON 14, 18 in main stat, 8 ancestry health)

--- TTRPG_Code/Character Builder.py
+++ TTRPG_Code/Character Builder.py
@@ -5,11 +5,26 @@ DamageFile = 'C:/Users/clays/Documents/GitHub/Personal-Projects/TTRPG_Code/Chara
 ACFile = 'C:/Users/clays/Documents/GitHub/Personal-Projects/TTRPG_Code/Character Increase Tables/Martial - Armor Class.csv'
 HPFile = 'C:/Users/clays/Documents/GitHub/Personal-Projects/TTRPG_Code/Character Increase Tables/Martial - HP.csv'
 
-attackdf = pd.read_csv(AttackFile)
-damagedf = pd.read_csv(DamageFile)
-ACdf = pd.read_csv(ACFile)
-HPdf = pd.read_csv(HPFile)
+files = [AttackFile, DamageFile, ACFile, HPFile]
+columnNames = ["Attack", "Damage", "AC", "HP"]
+for x in range(len(files)):
+    stringToChange = files[x]
+    files[x] = stringToChange.replace("clays", "sean")
 
-df = pd.concat([attackdf[cl], damagedf[cl], ACdf[cl], HPdf[cl]], axis=1)
+attackdf = pd.read_csv(files[0])
+damagedf = pd.read_csv(files[1])
+ACdf = pd.read_csv(files[2])
+HPdf = pd.read_csv(files[3])
+classes = list(attackdf.columns.values)
+classes.remove('Level')
 
-print(df)
+for x in classes:
+    classdf = pd.DataFrame([attackdf[x],damagedf[x],ACdf[x],HPdf[x]], columnNames)
+    classdf = classdf.transpose()
+    for c in range(3):
+        for b in range(20):
+            if b == 0:
+                classdf.iat[b,c] = classdf.iat[b,c]+1
+            else:
+                classdf.iat[b,c] = classdf.iat[b,c]+classdf.iat[b-1,c]+1
+    classdf.to_csv("Character Increase Tables/" + x + ".csv")
--- TTRPG_Code/Pathfinder Web Scraper.py
+++ TTRPG_Code/Pathfinder Web Scraper.py
@@ -1,6 +1,6 @@
 import bs4 as bs
 import requests
 
-URL = "https://pf2.d20pfsrd.com/monster/"
-r = requests.get(URL)
+mURL = "https://pf2.d20pfsrd.com/monster/"
+r = requests.get(mURL)
 print(r.content)
